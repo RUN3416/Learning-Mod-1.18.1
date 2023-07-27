9260d134d1e68944222bfcd3999efb81471c511d
Noah Davies <noah.davies.zoo@gmail.com>
2023-07-28 08:39:18 +1000

    I MADE AN ITEM!!!

--- src/main/java/net/noah/mccourse/MCCourseMod.java
+++ src/main/java/net/noah/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.noah.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.noah.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +11,10 @@ public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// a comment
 	@Override
 	public void onInitialize() {
 
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 
 }
