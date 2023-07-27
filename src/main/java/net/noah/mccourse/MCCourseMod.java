6eceda16b4abb0320d8c3ba971f6d8dbfbecd3f1
Noah Davies <noah.davies.zoo@gmail.com>
2023-07-28 07:48:23 +1000

    something happened

--- src/main/java/net/noah/mccourse/MCCourseMod.java
+++ src/main/java/net/noah/mccourse/MCCourseMod.java
@@ -10,6 +10,7 @@ public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// a comment
 	@Override
 	public void onInitialize() {
 
