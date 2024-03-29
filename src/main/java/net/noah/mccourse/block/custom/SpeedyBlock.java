f96417aedddc39bf1e238bb526045c85b0a4d88d
Noah Davies <noah.davies.zoo@gmail.com>
2024-03-29 11:11:40 -0600

    particles

--- src/main/java/net/noah/mccourse/block/custom/SpeedyBlock.java
+++ src/main/java/net/noah/mccourse/block/custom/SpeedyBlock.java
@@ -1,6 +1,7 @@
 package net.noah.mccourse.block.custom;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -8,11 +9,14 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class SpeedyBlock extends Block {
     public SpeedyBlock(Settings settings) {
@@ -31,6 +35,35 @@ public class SpeedyBlock extends Block {
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        float chance = 0.35f;
+
+        if(chance < random.nextFloat()) {
+            world.addParticle(ParticleTypes.SMOKE, pos.getX() + random.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + random.nextDouble(),
+                    0d, 0.015d + random.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < random.nextFloat()) {
+            world.addParticle(ParticleTypes.SMOKE, pos.getX() + random.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + random.nextDouble(),
+                    0d, 0.015d + random.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < random.nextFloat()) {
+            world.addParticle(ParticleTypes.SMOKE, pos.getX() + random.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + random.nextDouble(),
+                    0d, 0.015d + random.nextDouble(0.075d), 0d);
+        }
+
+        if(chance < random.nextFloat()) {
+            world.addParticle(ParticleTypes.SMOKE, pos.getX() + random.nextDouble(),
+                    pos.getY() + 0.5D, pos.getZ() + random.nextDouble(),
+                    0d, 0.015d + random.nextDouble(0.075d), 0d);
+        }
     }
 
     @Override
